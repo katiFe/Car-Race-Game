d6e4c7c17a7dc73d020ad2d2f61f5e44dc5b850a
Kati Fe <k-felgitsch@hotmail.com>
2021-10-01 09:49:38 +0100

    random size for obstacles and increase difficultie

--- classes.js
+++ classes.js
@@ -2,28 +2,46 @@ class Game {
     constructor(){
         this.currentTime = 0;
         this.car = null;
-        this.arrayOfObstacles = [];
+        this.obstacleArr = [];
     }
+
     startGame(){
         this.car = new Car();
         this.car.create();
         this.addEventListeners();
 
-
-
         setInterval( () => {
-            this.obstacle = new Obstacle(); //@todo: store in an array
-            this.obstacle.create();
-            this.arrayOfObstacles.push(this.obstacle);
-        }, 6000);
+            //update timer
+            this.currentTime++;
 
+            
+            this.obstacleArr.forEach( (obstacle) => {
+                //update obstacle positions
+                obstacle.moveDown();
+                obstacle.draw();
+                
+                
+                if(obstacle.y === 100) {
+                    //collision detection
+                    if(this.car.x < obstacle.x + obstacle.width && this.car.x + this.car.width > obstacle.x){
+                        alert("game over!");
+                    }
+                } else if(obstacle.y > 100) {
+                    //remove obstacles off the board
+                    obstacle.remove(); //remove from the DOM
+                    this.obstacleArr.shift(); //remove from our array of obstacles
+                }
 
-        setInterval( () => {
-            this.arrayOfObstacles.forEach((element) => {
-            element.moveDown(); 
-            element.draw();
             });
-        }, 1000);
+
+
+            //create new obstacles
+            if(this.currentTime % 5 === 0){
+                const newObstacle = new Obstacle();
+                newObstacle.create();
+                this.obstacleArr.push(newObstacle);
+            }
+        }, 200);
 
     }
 
@@ -31,10 +49,10 @@ class Game {
         document.addEventListener("keydown", (event) => {
             if(event.key === "ArrowLeft"){
                 this.car.moveLeft();
-                this.car.draw(); //@todo
+                this.car.draw();
             } else if (event.key === "ArrowRight") {
                 this.car.moveRight();
-                this.car.draw(); //@todo
+                this.car.draw();
             }
         });
     }
@@ -46,12 +64,15 @@ class Game {
 class Thing {
     constructor(){
         this.domElm = null;
+        this.gameElm = document.getElementById("game");
     }
     create(){
         this.domElm = document.createElement("div");
         this.domElm.className = this.className;
-        const gameElm = document.getElementById("game");        
-        gameElm.appendChild(this.domElm);
+        this.gameElm.appendChild(this.domElm);
+    }
+    remove(){
+        this.gameElm.removeChild(this.domElm);
     }
     draw(){
         this.domElm.style.width = this.width + "%";
@@ -64,10 +85,11 @@ class Thing {
 class Car extends Thing {
     constructor(){
         super();
-        this.x = 50;
-        this.y = 100;
         this.width = 10;
         this.height = 20;
+        this.x = 50;
+        this.y = 100;
+        
         this.className = "car";
     }
     moveLeft(){
@@ -82,13 +104,18 @@ class Car extends Thing {
 class Obstacle extends Thing {
     constructor(){
         super();
-        this.x = 50;
+        this.width = Math.floor(Math.random() * (30 - 5 + 1) + 5); ; //random size between 5 and 20
+        this.height = this.width / 2;
+        this.x = Math.floor(Math.random() * (100 - this.width + 1)); //random postion between 0 and 100 - this width
         this.y = 0;
-        this.width = 20;
-        this.height = 5;
+        
         this.className = "obstacle";
     }
     moveDown(){
         this.y = this.y + 5 ;
     }
+
+        
 }
+   
+
